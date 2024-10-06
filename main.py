12a9cea08afe86e3e9ee89748221d546e4ee8e50
atiozn <atifoezen@googlemail.com>
2024-10-06 13:01:48 +0200

    Added function to sort the occurence of characters in descending order

--- main.py
+++ main.py
@@ -16,20 +16,36 @@ def count_chars(words):
     dict = {}
     words = words.lower()
     for char in words:
-        if char in dict:
-                dict[char] += 1
-        else:
-                dict[char] = 1
+        if char.isalpha():
+            if char in dict:
+                    dict[char] += 1
+            else:
+                    dict[char] = 1
     return dict
 
 def create_report(count_words, count_chars):
     print("--- Begin report of books/frankenstein.txt ---")       
     print(f"{count_words} words found in the document")
-    sorted(count_chars)
     
-    for char in count_chars:
-           print(f"The {char} character was found {count_chars[char]} times")
+    converted_list = dict_to_list(count_chars)
+    converted_list.sort(reverse=True, key=sort_on)
+
+    for dict in converted_list:
+        for key in dict:
+           print(f"The {key} character was found {dict[key]} times")
 
     print("--- End report ---")
+
+def sort_on(dict):
+       for c in dict:
+             return dict[c]
+ 
+def dict_to_list(dict):
+    list_of_dict = []
+    for key in dict:
+          new_dict = {key:dict[key]}
+          list_of_dict.append(new_dict)
+    return list_of_dict
+       
 
 main()
